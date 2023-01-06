d2498c89b171d5322971f715d4124f1a47cf71fb
mokupsas <mockus.ri@gmail.com>
2023-01-06 12:28:36 +0200

    update adaptive Pagination numbering

--- website/src/components/Pagination.vue
+++ website/src/components/Pagination.vue
@@ -7,7 +7,7 @@
         -->
         <ul class="pagination-list">
             <li v-for="page in parseInt(this.pages)">
-                <div v-if="page <= 10">
+                <div v-if="buttonLimit(page)">
                     <router-link v-if="page == current" :to="{name: `Page`, params: { page: page}}" class="pagination-link is-current">{{ page }}</router-link>
                     <router-link v-else :to="{name: `Page`, params: { page: page}}" class="pagination-link">{{ page }}</router-link>
                 </div>
@@ -20,17 +20,50 @@
 <script>
 export default {
     props: {
-        current: { default: 1 },   // current page
+        current: { default: 1 },    // current page
         pages: { default: 1 }      // total pages 
     },
+    data() {
+        return {
+            btnLimit: 9
+        }
+    },
     created() {
-        console.log(this.current)
-        console.log(this.pages)
-
+        //console.log(Math.floor(this.btnLimit / 2))
+        //console.log(this.current)
+        console.log(this.leftOffset(4))
+        //console.log(this.rightOffset(4))  
     },
     methods: {
         getPages() {
             return this.pages;
+        },
+        leftOffset(oneSide) {
+            /*
+            console.log('-')
+            console.log(this.current)
+            console.log(this.pages - oneSide)
+            */
+            if(this.current > this.pages - oneSide)   
+                return this.current - this.pages + oneSide;
+            return 0;
+        },
+        rightOffset(oneSide) {
+            if(this.current <= oneSide)   
+                return oneSide + 1 - this.current;
+            return 0;
+        },
+        buttonLimit(page) {
+            /*  Pagination structre
+                (left side) (center - current page) (right side)
+            */
+            let oneSide = Math.floor(this.btnLimit / 2);
+            let min = this.current - oneSide - this.leftOffset(oneSide);
+            let max = this.current + oneSide + this.rightOffset(oneSide);
+
+            if(page >= min && page <= max)
+                return true;
+            return false;
         }
     }
 }
